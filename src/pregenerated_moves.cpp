1548d953dbd6d278486fb386cf5a1267026331a1
leofracca <leonardofraccaroli1999@gmail.com>
2025-07-08 00:19:25 +0200

    Simplify bit operations for pregenerated bitboard attacks

--- src/pregenerated_moves.cpp
+++ src/pregenerated_moves.cpp
@@ -9,31 +9,13 @@ namespace chess_engine
 
     if (side == Side::White)
     {
-        // Check if the pawn is on the a file to avoid out-of-bounds attacks
-        if (((bitboard >> 7) & NOT_A_FILE) != Bitboard())
-        {
-            attacks |= bitboard >> 7; // Attack left diagonal
-        }
-
-        // Check if the pawn is on the h file to avoid out-of-bounds attacks
-        if (((bitboard >> 9) & NOT_H_FILE) != Bitboard())
-        {
-            attacks |= bitboard >> 9; // Attack right diagonal
-        }
+        attacks |= (bitboard >> 7) & NOT_A_FILE; // Attack left diagonal
+        attacks |= (bitboard >> 9) & NOT_H_FILE; // Attack right diagonal
     }
     else // Black pawns
     {
-        // Check if the pawn is on the h file to avoid out-of-bounds attacks
-        if (((bitboard << 7) & NOT_H_FILE) != Bitboard())
-        {
-            attacks |= bitboard << 7; // Attack right diagonal
-        }
-
-        // Check if the pawn is on the a file to avoid out-of-bounds attacks
-        if (((bitboard << 9) & NOT_A_FILE) != Bitboard())
-        {
-            attacks |= bitboard << 9; // Attack left diagonal
-        }
+        attacks |= (bitboard << 7) & NOT_H_FILE; // Attack right diagonal
+        attacks |= (bitboard << 9) & NOT_A_FILE; // Attack left diagonal
     }
 
     return attacks;
@@ -45,46 +27,16 @@ namespace chess_engine
     Bitboard attacks;
 
     // Upward attacks
-    if (((bitboard >> 17) & NOT_H_FILE) != Bitboard())
-    {
-        attacks |= bitboard >> 17; // Attack 2 squares up and 1 square left
-    }
-
-    if (((bitboard >> 15) & NOT_A_FILE) != Bitboard())
-    {
-        attacks |= bitboard >> 15; // Attack 2 squares up and 1 square right
-    }
-
-    if (((bitboard >> 10) & NOT_GH_FILE) != Bitboard())
-    {
-        attacks |= bitboard >> 10; // Attack 1 square up and 2 squares left
-    }
-
-    if (((bitboard >> 6) & NOT_AB_FILE) != Bitboard())
-    {
-        attacks |= bitboard >> 6; // Attack 1 square up and 2 squares right
-    }
+    attacks |= (bitboard >> 17) & NOT_H_FILE; // Attack 2 squares up and 1 square left
+    attacks |= (bitboard >> 15) & NOT_A_FILE; // Attack 2 squares up and 1 square right
+    attacks |= (bitboard >> 10) & NOT_GH_FILE; // Attack 1 square up and 2 squares left
+    attacks |= (bitboard >> 6) & NOT_AB_FILE; // Attack 1 square up and 2 squares right
 
     // Downward attacks
-    if (((bitboard << 17) & NOT_A_FILE) != Bitboard())
-    {
-        attacks |= bitboard << 17; // Attack 2 squares down and 1 square right
-    }
-
-    if (((bitboard << 15) & NOT_H_FILE) != Bitboard())
-    {
-        attacks |= bitboard << 15; // Attack 2 squares down and 1 square left
-    }
-
-    if (((bitboard << 10) & NOT_AB_FILE) != Bitboard())
-    {
-        attacks |= bitboard << 10; // Attack 1 square down and 2 squares right
-    }
-
-    if (((bitboard << 6) & NOT_GH_FILE) != Bitboard())
-    {
-        attacks |= bitboard << 6; // Attack 1 square down and 2 squares left
-    }
+    attacks |= (bitboard << 17) & NOT_A_FILE; // Attack 2 squares down and 1 square right
+    attacks |= (bitboard << 15) & NOT_H_FILE; // Attack 2 squares down and 1 square left
+    attacks |= (bitboard << 10) & NOT_AB_FILE; // Attack 1 square down and 2 squares right
+    attacks |= (bitboard << 6) & NOT_GH_FILE; // Attack 1 square down and 2 squares left
 
     return attacks;
 }
